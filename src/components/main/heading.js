49b780cb9934fc6c804613eaed67c63ce0429433
xKarol <karolpiskorz8@gmail.com>
2022-06-02 18:03:04 +0200

    Fix subheading animation bug

--- src/components/main/heading.js
+++ src/components/main/heading.js
@@ -3,23 +3,19 @@ import { StyledHeadingBox, StyledHeading, StyledSubheading } from "./styles";
 function Heading({ heading, subheading }) {
   return (
     <StyledHeadingBox>
-      {heading?.length && (
-        <StyledHeading
-          initial={{ opacity: 0, translateY: 25 }}
-          animate={{ opacity: 1, translateY: 0 }}
-          transition={{ delay: 0.5, duration: 0.6 }}
-        >
-          {heading}
-        </StyledHeading>
-      )}
-      {subheading?.length && (
-        <StyledSubheading
-          initial={{ opacity: 0, translateY: 25 }}
-          animate={{ opacity: 1, translateY: 0 }}
-          transition={{ delay: 1.1, duration: 0.6 }}
-          dangerouslySetInnerHTML={{ __html: subheading }}
-        ></StyledSubheading>
-      )}
+      <StyledHeading
+        initial={{ opacity: 0, translateY: 25 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ delay: 0.5, duration: 0.6 }}
+      >
+        {heading}
+      </StyledHeading>
+      <StyledSubheading
+        initial={{ opacity: 0, translateY: 25 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ delay: 1.1, duration: 0.6 }}
+        dangerouslySetInnerHTML={{ __html: subheading }}
+      ></StyledSubheading>
     </StyledHeadingBox>
   );
 }
